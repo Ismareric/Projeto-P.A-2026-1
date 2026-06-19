eed605b2b7750df178c177dcd31c269a41b658a7
spiralout <dabrab20@gmail.com>
2026-06-19 09:41:18 -0300

    Consegui abrir um tela com area de desenha(que nao desenha)

--- Main.py
+++ Main.py
@@ -1,6 +1,12 @@
 from tkinter import *
 from tkinter import ttk
+#teoricamente você poderia digitar toda vez que colocasse um widget no frmae mas me poupe
+espacaomento={'padx':5,'pady':5}
 janela=Tk()
-label=Label(janela,text='!!!')
-label.grid(column=0,row=0)
+janelapropria=Frame(janela,width=1280,height=720)
+texto1=ttk.Label(janelapropria,text='desenhos')
+texto1.grid(column=0,row=0,**espacaomento)
+desenho=Canvas(janelapropria,width=1280,height=720,bg='white')
+desenho.grid(column=0,row=1,**espacaomento)
+janelapropria.pack()
 janela.mainloop()
