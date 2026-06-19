f8b8d70824c0ff3f5ae4d3665b0c446b572d0310
spiralout <dabrab20@gmail.com>
2026-06-19 09:41:44 -0300

    2

--- Main.py
+++ Main.py
@@ -2,11 +2,16 @@ from tkinter import *
 from tkinter import ttk
 #teoricamente você poderia digitar toda vez que colocasse um widget no frmae mas me poupe
 espacaomento={'padx':5,'pady':5}
+
 janela=Tk()
 janelapropria=Frame(janela,width=1280,height=720)
+
 texto1=ttk.Label(janelapropria,text='desenhos')
 texto1.grid(column=0,row=0,**espacaomento)
+
 desenho=Canvas(janelapropria,width=1280,height=720,bg='white')
 desenho.grid(column=0,row=1,**espacaomento)
+
+
 janelapropria.pack()
 janela.mainloop()
