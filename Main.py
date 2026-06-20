dcf776615903a3e29b17b0239d160ed4bee2d949
spiralout <dabrab20@gmail.com>
2026-06-20 20:56:07 -0300

    1

--- Main.py
+++ Main.py
@@ -7,6 +7,7 @@ class figuras:
     def __init__(self):
         self.figuras=[]
     ##polemica do desenhar_figuras! perdeu tudo e ta morando de aluguel como método da classe figuras
+    ##cor 1 é o outline, cor 2 é o preenchimento (SUJEITO A MUDANÇA)
     def desenhar_figuras(self):
         desenho.delete("all")
         for fig, values,cor1,cor2 in self.figuras:
@@ -21,6 +22,19 @@ class figuras:
             else : #circulo
                 raio = ((values[2] - values[0])**2 + (values[3]- values[1])**2)**0.5
                 desenho.create_oval(values[0] - raio, values[1] - raio, values[0]+raio, values[1] + raio,outline=cor1,fill=cor2)
+    def desenhar_figura_nova(self):
+        fig, values,cor1,cor2 = fig_nova
+        if fig == "linha":
+            desenho.create_line(values[0], values[1], values[2], values[3], dash=(4, 2),fill=cor1)
+        elif fig == "rabisco":
+            desenho.create_line(values, dash=(4, 2),fill=cor1)
+        elif fig == "retângulo":
+            desenho.create_rectangle(values[0], values[1], values[2], values[3], dash=(4, 2),outline=cor1,fill=cor2)
+        elif fig == 'oval':
+            desenho.create_oval(values[0], values[1], values[2], values[3], dash=(4, 2),outline=cor1,fill=cor2)
+        else : #circulo
+            raio = ((values[2] - values[0])**2 + (values[3]- values[1])**2)**0.5
+            desenho.create_oval(values[0] - raio, values[1] - raio, values[0]+raio, values[1] + raio, dash=(4, 2),outline=cor1,fill=cor2)
 #Identifica qual figura será desenhada
 def iniciar_figura_nova(event): 
     global fig_nova
@@ -51,28 +65,15 @@ def atualizar_figura_nova(event):
         fig_nova = ('circulo', (fig_nova[1][0], fig_nova[1][1], event.x, event.y), fig_nova[2], fig_nova[3])
         
     figs.desenhar_figuras()
-    desenhar_figura_nova()
+    figs.desenhar_figura_nova()
 
 # Quando mouse é solto
 def incluir_figura_nova(event): 
     if not incompleta(fig_nova): # para evitar incluir figuras incompletas, como uma linha sem comprimento ou um rabisco com um único ponto
         figs.figuras.append(fig_nova) 
     figs.desenhar_figuras()
-##cor 1 é o outline, cor 2 é o preenchimento
 
-def desenhar_figura_nova():
-    fig, values,cor1,cor2 = fig_nova
-    if fig == "linha":
-        desenho.create_line(values[0], values[1], values[2], values[3], dash=(4, 2),fill=cor1)
-    elif fig == "rabisco":
-        desenho.create_line(values, dash=(4, 2),fill=cor1)
-    elif fig == "retângulo":
-        desenho.create_rectangle(values[0], values[1], values[2], values[3], dash=(4, 2),outline=cor1,fill=cor2)
-    elif fig == 'oval':
-        desenho.create_oval(values[0], values[1], values[2], values[3], dash=(4, 2),outline=cor1,fill=cor2)
-    else : #circulo
-        raio = ((values[2] - values[0])**2 + (values[3]- values[1])**2)**0.5
-        desenho.create_oval(values[0] - raio, values[1] - raio, values[0]+raio, values[1] + raio, dash=(4, 2),outline=cor1,fill=cor2)
+
 
 def incompleta(figura):
     fig, values,cor1,cor2 = figura
