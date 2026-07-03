c2fc1c6d71e8d5f03bcc72f4cec97b4bc005b9d4
luuismeneses1 <luuismeneses1@gmail.com>
2026-07-02 22:02:15 -0300

    Continuando o State

--- src/PROJETO-P.A-2026-1/controlador/estados.py
+++ src/PROJETO-P.A-2026-1/controlador/estados.py
@@ -3,12 +3,15 @@ from modelo.formas import *
 
 class EstadoFerramenta(ABC):
     @abstractmethod
-    def ao_clicar(self, contolador, event):
+    def ao_clicar(self, controlador, event):
         pass
+    @abstractmethod
     def ao_arrastar(self, controlador, event):
         pass
+    @abstractmethod
     def ao_soltar(self, controlador, event):
         pass
+    @abstractmethod
     def ao_clicar_direito(self, controlador, event):
         pass
 
@@ -25,6 +28,63 @@ class EstadoLinha(EstadoFerramenta):
             controlador.visao.desenhar_incompleto('Linha',
                                                   controlador.fig_nova.coord,
                                                   controlador.cordeoutline[1]
+                                                  controlador.cordepreenchimento[1])
+    
+    def ao_soltar(self, controlador, event):
+        if controlador.incompleta(controlador.fig_nova):
+            controlador.modelo.figuras.append(controlador.fig_nova)
+        controlador.fig_nova = None
+        controlador.visao.desenhar_todas(controlador.modelo.figuras)
+
+class EstadoRabisco(EstadoFerramenta):
+    def ao_clicar(self, controlador, event):
+        controlador.fig_nova = Rabisco(event, controlador.cordeoutline)
+    
+    def ao_arrastar(self, controlador, event):
+        if controlador.fig_nova != None:
+            controlador.fig_nova.atualizar_figura_nova(event)
+            controlador.visao.desenhar_todas(controlador.modelo.figuras)
+            controlador.visao.desenhar_incompleto('Rabisco',
+                                                  controlador.fig_nova.coord,
+                                                  controlador.cordeoutline[1],
+                                                  controlador.cordepreenchimento[1])
+    
+    def ao_soltar(self, controlador, event):
+        if controlador.incompleta(controlador.fig_nova):
+            controlador.modelo.figuras.append(controlador.fig_nova)
+        controlador.fig_nova = None
+        controlador.visao.desenhar_todas(controlador.modelo.figuras)
+
+class EstadoRetangulo(EstadoFerramenta):
+    def ao_clicar(self, controlador, event):
+        controlador.fig_nova = Retangulo(event, controlador.cordeoutline)
+    
+    def ao_arrastar(self, controlador, event):
+        if controlador.fig_nova != None:
+            controlador.fig_nova.atualizar_figura_nova(event)
+            controlador.visao.desenhar_todas(controlador.modelo.figuras)
+            controlador.visao.desenhar_incompleto('Retângulo',
+                                                  controlador.fig_nova.coord,
+                                                  controlador.cordeoutline[1],
+                                                  controlador.cordepreenchimento[1])
+    
+    def ao_soltar(self, controlador, event):
+        if controlador.incompleta(controlador.fig_nova):
+            controlador.modelo.figuras.append(controlador.fig_nova)
+        controlador.fig_nova = None
+        controlador.visao.desenhar_todas(controlador.modelo.figuras)
+
+class EstadoOval(EstadoFerramenta):
+    def ao_clicar(self, controlador, event):
+        controlador.fig_nova = Rabisco(event, controlador.cordeoutline)
+    
+    def ao_arrastar(self, controlador, event):
+        if controlador.fig_nova != None:
+            controlador.fig_nova.atualizar_figura_nova(event)
+            controlador.visao.desenhar_todas(controlador.modelo.figuras)
+            controlador.visao.desenhar_incompleto('Oval',
+                                                  controlador.fig_nova.coord,
+                                                  controlador.cordeoutline[1],
                                                   controlador.cordepreenchimento[1])
     
     def ao_soltar(self, controlador, event):
